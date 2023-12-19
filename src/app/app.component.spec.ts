44cdf4bdc59ce1a4ad0340cc3210a800231e4c52
Artem Fedorkevich <artem.fedorkevich@gmail.com>
2023-12-19 13:06:58 +0100

    Code fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'untitled'`, () => {
+  it(`should have as title 'TMS-Shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('untitled');
+    expect(app.title).toEqual('TMS-Shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, untitled');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TMS-Shop');
   });
 });
