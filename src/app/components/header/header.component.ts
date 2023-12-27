64541732acdd8fc3f2279f73bc5615353d854e35
Artem Fedorkevich <artem.fedorkevich@gmail.com>
2023-12-27 10:09:51 +0100

    A1 Add black theme

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,36 @@ import { Component, Input } from '@angular/core';
 })
 export class HeaderComponent {
   @Input() themeColor: 'black' | 'white' = 'white';
+  imagePaths: { black: { bx_menu_alt_right_image: string; carbon_favorite_image: string; shopping_card_image: string; user_image: string }; white: { bx_menu_alt_right_image: string; carbon_favorite_image: string; shopping_card_image: string; user_image: string } } = {
+    black: {
+      bx_menu_alt_right_image: '../../../assets/images/bx_menu_alt_right_black.png',
+      carbon_favorite_image: '../../../assets/images/carbon_favorite_black.png',
+      shopping_card_image: '../../../assets/images/shopping_card_black.png',
+      user_image: '../../../assets/images/user_black.png'
+    },
+    white: {
+      bx_menu_alt_right_image: '../../../assets/images/bx_menu_alt_right_white.png',
+      carbon_favorite_image: '../../../assets/images/carbon_favorite_white.png',
+      shopping_card_image: '../../../assets/images/shopping_card_white.png',
+      user_image: '../../../assets/images/user_white.png'
+    }
+  };
+   imageSources: {
+    bx_menu_alt_right_image: string;
+    carbon_favorite_image: string;
+    shopping_card_image: string;
+    user_image: string;
+  } | undefined;
+
+  constructor() {
+    this.setImageSources();
+  }
+
+  ngOnChanges() {
+    this.setImageSources();
+  }
+
+  private setImageSources() {
+    this.imageSources = this.imagePaths[this.themeColor];
+  }
 }
